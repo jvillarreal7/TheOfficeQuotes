dc53f8a93e5d66dda731bae10c303080710f8756
Jaime Villarreal <jvillarreal337@gmail.com>
2020-03-16 00:16:15 -0600

    Changes: - Django admin page now operational. - Model adjustments. - Base template adjustments. - Ignore site content.

--- OfficeQts/OfficeApp/models.py
+++ OfficeQts/OfficeApp/models.py
@@ -12,8 +12,9 @@ class AbstractModel(models.Model):
     """General attributes that consequent models will have as well."""
     created_by = models.PositiveIntegerField(default=None)
     created_date = models.DateTimeField(default=timezone.now)
-    modified_by = models.PositiveIntegerField(default=None)
-    modified_date = models.DateTimeField(default=None)
+    modified_by = models.PositiveIntegerField(
+        default=None, blank=True, null=True)
+    modified_date = models.DateTimeField(default=None, blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
     class Meta:
@@ -28,11 +29,17 @@ class Quote(AbstractModel):
 
     content = models.CharField(max_length=999)
 
+    def __str__(self):
+        return self.character.first_name + " " + self.character.last_name + ": " + self.content
+
 
 class Actor(AbstractModel):
     """Models that represents any actor from the show."""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 
 class Character(AbstractModel):
@@ -43,24 +50,39 @@ class Character(AbstractModel):
     last_name = models.CharField(max_length=100)
     picture = models.ImageField(upload_to='characters/', default=None)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 
 class Season(AbstractModel):
     """Model that represents a season of the show."""
     number = models.PositiveIntegerField()
 
+    def __str__(self):
+        return "Season " + str(self.number)
+
 
 class Episode(AbstractModel):
     """Model that represents an episode of the show."""
-    written_by = models.ForeignKey(
-        "StaffMember", default=None, on_delete=models.CASCADE, related_name='episodes_w')
     directed_by = models.ForeignKey(
-        "StaffMember", default=None, on_delete=models.CASCADE, related_name='episodes_d')
+        "StaffMember", default=None, null=True, blank=True, on_delete=models.CASCADE, related_name='episodes_d')
+    written_by = models.ForeignKey(
+        "StaffMember", default=None, null=True, blank=True, on_delete=models.CASCADE, related_name='episodes_w')
+    season = models.ForeignKey("Season", on_delete=models.CASCADE)
 
+    name = models.CharField(max_length=100)
     number = models.PositiveIntegerField()
-    air_date = models.DateTimeField(default=None)
+    air_date = models.DateField(default=None)
+    running_time = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "Season " + str(self.season.number) + " / " + "Episode " + str(self.number) + ": " + self.name
 
 
 class StaffMember(AbstractModel):
     """Model that represents any staff member of the show."""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
